f37e5c9f0523c42e628e67747ba8f66d183f0f30
montassier-paul <paul.montassier.pro@gmail.com>
2022-05-06 11:53:31 +0000

    lab 3 code

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
@@ -18,7 +19,7 @@ urlpatterns = [
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
     # <HINT> Create a route for submit view
-
+    path('<int:course_id>/submit/', views.submit, name='submit'),
     # <HINT> Create a route for show_exam_result view
-
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
